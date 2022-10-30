f3600bd32bae747cf137cdbc534012e47eee856f
Tanndlin <tanndlin@gmail.com>
2022-10-30 10:14:05 -0400

    Reworded Projects

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -6,14 +6,14 @@ import Project from '../components/Project';
 
 function ProjectsPage() {
     return (
-        <div className='bg-sky-500 min-h-minus-header flex flex-col gap-4 justify'>
+        <div className='bg-sky-500 min-h-minus-header flex flex-col gap-16'>
 
-            <article className="container bg-blue-700 m-auto mt-16 rounded-md text-white p-3">
+            <article className="container bg-blue-700 m-auto mb-0 mt-16 rounded-md text-white p-3">
                 <h1 className="text-7xl text-center">Personal Projects</h1>
                 <Project title={'Lion'} link={'https://github.com/cs-discord-at-ucf/lion'}>
-                    <li>Lead Developer of Lion, a discord bot developed in TypeScript</li>
-                    <li>Helps manage the UCF CS Discord, making the life of the average user and moderator easier</li>
-                    <li>Serves 10,000+ users</li>
+                    <li>Lead Developer of Lion, a discord bot developed in TypeScript with over 30 contributors</li>
+                    <li>Helps manage the UCF CS Discord, making the life of the average user and moderator easier, serving <b>10,000+</b> users</li>
+                    <li>25,000 line diff since I joined the project in January 2021</li>
                 </Project>
 
                 <Project title={'Contact Manager'} link={'https://github.com/tanndlin/Contact-Manager'}>
@@ -22,11 +22,11 @@ function ProjectsPage() {
                 </Project>
             </article>
 
-            <article className="container bg-blue-700 m-auto mb-16 rounded-md text-white p-3">
+            <article className="container bg-blue-700 m-auto mb-16 mt-0 rounded-md text-white p-3">
                 <h1 className="text-7xl text-center">Experience</h1>
                 <Experience title={'Georgia Tech Research Institute'} date={'Oct 2022 - Present'}>
                     <li>Software Engineer Intern</li>
-                    <li>C# Frontend Developer for .NET GUIs</li>
+                    <li>C# Frontend Developer using .NET framework to create GUIs for desktop applications</li>
                 </Experience>
             </article>
 
