55886dadad84e4b126443311f768d86951bc9b27
Tanndlin <tanndlin@gmail.com>
2022-11-30 20:35:50 -0500

    Add Budgie

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,14 +5,14 @@ function Project(props) {
     return (
         <section className=''>
             <div>
-                <h1 className={`text-2xl mb-16 ${props.side === 'right' && 'text-right'}`}>
+                <h1 className={`text-2xl mb-16 font-mono ${props.side === 'right' && 'text-right'}`}>
                     {props.title}
                 </h1>
                 <div className='relative'>
-                    <ul className={`${props.side === 'right' && 'ml-auto'} list-disc pl-8 w-3/5 p-4 bg-gray-800 rounded-md shadow-xl`}>
+                    <ul className={`${props.side === 'right' ? 'ml-auto' : ''} list-disc pl-8 w-3/5 p-4 bg-gray-800 rounded-md shadow-xl`}>
                         {props.children}
                     </ul>
-                    <footer className={`${props.side === 'right' && 'float-right'} flex flex-col mt-4`}>
+                    <footer className={`${props.side === 'right' ? 'float-right' : ''} flex flex-col mt-4`}>
                         <ul className={`flex gap-4 text-gray-200`}>
                             {
                                 props.technologies.map((tech, index) => {
@@ -24,12 +24,12 @@ function Project(props) {
                                 })
                             }
                         </ul>
-                        <a className={`${props.side === 'right' && 'justify-end'} flex mt-4 gap-4`} href={props.link}>
-                            <Icon className={`${props.side === 'right' && 'right-0'} w-8`} name='github' />
+                        <a className={`${props.side === 'right' ? 'justify-end' : ''} flex mt-4 gap-4 w-min h-min hover:fill-white`} href={props.link}>
+                            <Icon className={`${props.side === 'right' ? 'right-0' : ''} w-8`} name='github' />
                         </a>
                     </footer>
                     <a
-                        className={`max-w-[500px] absolute top-1/2 -translate-y-1/2 -z-10 ${props.side === 'left' ? 'right-0' : 'left-0'}`}
+                        className={`max-w-[500px] absolute top-1/2 -translate-y-1/2 -z-[1] ${props.side === 'left' ? 'right-0' : 'left-0'}`}
                         href={props.link}>
                         <img className='min-h-[237px]' src={props.img} alt="Project" />
                     </a>
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Project from '../components/Project';
 import lionImg from '../img/lion.png';
 import contactManagerImg from '../img/contact-manager-ss.png';
+import budgie from '../img/budgie.png';
 
 function ProjectsPage() {
     return (
         <div id='projects' className='min-h-screen flex w-full'>
 
             <article className="container w-full m-auto text-white flex flex-col gap-8">
-                <h1 className="text-3xl">Personal Projects</h1>
+                <h1 className="text-4xl mb-24 font-mono">Personal Projects</h1>
+
+                <Project
+                    title='Budgie'
+                    link='https://budgie.live'
+                    img={budgie}
+                    side='right'
+                    technologies={['React', 'Firebase', 'Flutter', 'ExpressJS']}
+                >
+                    <p className='font-mono'>
+                        A budgie tracker app that allows users to keep track of
+                        recurring bills, grocery budgets, and one time expenses.
+                        I was the lead front end eningeer for the website and
+                        helped oversee the development of the expressJS API.
+                    </p>
+                </Project>
+
                 <Project
                     title='Lion'
                     link='https://github.com/cs-discord-at-ucf/lion'
@@ -16,7 +33,7 @@ function ProjectsPage() {
                     side='left'
                     technologies={["TypeScript", "Discord API", "MongoDB", "Docker"]}
                 >
-                    <p>
+                    <p className='font-mono'>
                         Lead Developer of Lion, a discord bot developed in
                         TypeScript with over 30 contributors. Helps manage the
                         UCF CS Discord, making the life of the average user and
@@ -31,7 +48,7 @@ function ProjectsPage() {
                     side='right'
                     technologies={["Pure HTML/CSS", "JavaScript", "SQL", "Php"]}
                 >
-                    <p>
+                    <p className='font-mono'>
                         Sole frontend eningeer on a fullstack website application
                         for managing a user's contacts <br />
                         <a href='/cmindex.html' target="_blank" rel="noopener noreferrer"> <b>Check Me Out</b> (Some Features will not work as I adapted this to work without a backend and API)</a>
