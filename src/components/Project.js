c991267258b8a3e526c433c7b0c1726e5d602cd3
Tanndlin <tanndlin@gmail.com>
2022-12-17 21:26:52 -0500

    Move alternation to scss

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,37 +3,35 @@ import Icon from './Icon';
 
 function Project(props) {
     return (
-        <section className=''>
-            <div>
-                <h1 className={`text-2xl mb-16 font-mono ${props.side === 'right' && 'text-right'}`}>
-                    {props.title}
-                </h1>
-                <div className='relative'>
-                    <ul className={`${props.side === 'right' ? 'ml-auto' : ''} list-disc pl-8 w-3/5 p-4 bg-gray-800 rounded-md shadow-xl`}>
-                        {props.children}
+        <section className='project'>
+            <h1 className={`text-2xl mb-16 font-mono`}>
+                {props.title}
+            </h1>
+            <div className='relative'>
+                <ul className={`description list-disc pl-8 w-3/5 p-4 bg-gray-800 rounded-md shadow-xl`}>
+                    {props.children}
+                </ul>
+                <footer className={`techContainer flex flex-col mt-4`}>
+                    <ul className={`flex gap-4 text-gray-200`}>
+                        {
+                            props.technologies.map((tech, index) => {
+                                return (
+                                    <li key={index}>
+                                        {tech}
+                                    </li>
+                                );
+                            })
+                        }
                     </ul>
-                    <footer className={`${props.side === 'right' ? 'float-right' : ''} flex flex-col mt-4`}>
-                        <ul className={`flex gap-4 text-gray-200`}>
-                            {
-                                props.technologies.map((tech, index) => {
-                                    return (
-                                        <li key={index}>
-                                            {tech}
-                                        </li>
-                                    );
-                                })
-                            }
-                        </ul>
-                        <a className={`${props.side === 'right' ? 'justify-end' : ''} flex mt-4 gap-4 w-min h-min hover:fill-white`} href={props.link}>
-                            <Icon className={`${props.side === 'right' ? 'right-0' : ''} w-8`} name='github' />
-                        </a>
-                    </footer>
-                    <a
-                        className={`max-w-[500px] absolute top-1/2 -translate-y-1/2 -z-[1] ${props.side === 'left' ? 'right-0' : 'left-0'}`}
-                        href={props.link}>
-                        <img className='min-h-[237px]' src={props.img} alt="Project" />
+                    <a className={`projectLink flex mt-4 gap-4 w-min h-min hover:fill-white`} href={props.link}>
+                        <Icon className={`w-8`} name='github' />
                     </a>
-                </div>
+                </footer>
+                <a
+                    className={`max-w-[500px] absolute top-1/2 -translate-y-1/2 -z-[1] ${props.side === 'left' ? 'right-0' : 'left-0'}`}
+                    href={props.link}>
+                    <img className='min-h-[237px]' src={props.img} alt="Project" />
+                </a>
             </div>
         </section>
     );
