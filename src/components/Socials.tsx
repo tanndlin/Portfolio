fc8afd5474ff804b9e02e31dfe9717882db7afde
tanndlin <tanndlin@gmail.com>
2025-02-13 18:53:29 -0500

    Remove Twitter

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import SocialLink from './SocialLink';
 import githubLogo from '../img/github.png';
-import twitterLogo from '../img/twitter.png';
 import linkedinLogo from '../img/linkedin.png';
+import SocialLink from './SocialLink';
 
 const Socials = () => {
     return (
         <ul
             id="socialsContainer"
-            className="fixed flex flex-col justify-center gap-4 bottom-16 ml-10"
+            className="fixed flex flex-col justify-center gap-4 ml-10 bottom-16"
         >
             <li>
                 <SocialLink
                     link={'https://github.com/tanndlin'}
                     logo={githubLogo}
                     alt={'Github Logo'}
-                />
-            </li>
-            <li>
-                <SocialLink
-                    link={'https://twitter.com/Tanner_Sandlin'}
-                    logo={twitterLogo}
-                    alt={'Twitter Logo'}
                 />
             </li>
             <li>
