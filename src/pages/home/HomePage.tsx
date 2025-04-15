2f1b42f1a71a1584ce3d46c3bb968bfd37d9f634
tanndlin <tanndlin@gmail.com>
2025-04-14 21:37:57 -0400

    Center home page text

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 function HomePage() {
     return (
-        <article id="home" className="flex-1 w-full my-auto">
-            <h2 className="font-mono text-3xl">Hi, my name is</h2>
-            <h1 className="font-bold text-7xl transition-header">
-                Tanner Sandlin
-            </h1>
-            <p className="mt-4 text-xl">
-                I am a software engineer and a CS grad from the University of
-                Central Florida.
-            </p>
+        <article id="home" className="flex flex-1 w-full mx-auto">
+            <div className="mx-auto">
+                <h2 className="font-mono text-3xl">Hi, my name is</h2>
+                <h1 className="font-bold text-7xl transition-header">
+                    Tanner Sandlin
+                </h1>
+                <p className="mt-4 text-xl">
+                    I am a software engineer and a CS grad from the University
+                    of Central Florida.
+                </p>
+            </div>
         </article>
     );
 }
